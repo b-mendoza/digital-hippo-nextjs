6b0e80e35551f468a413b49a20daf4df9a8b2870
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-31 17:26:14 -0600

    fix(page): rename Home component to RootPage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import { MaxWidthWrapper } from '~/components/max-width-wrapper';
 
-export default function Home() {
+export default function RootPage() {
   return (
     <MaxWidthWrapper>
       <div className="mx-auto flex max-w-3xl flex-col items-center py-20 text-center">
