7420ae6b40430569d8b7850661c0f0c022383244
Bryan Mendoza <bmendoza.dev@hotmail.com>
2023-12-31 16:24:26 -0600

    feat(linting): add `lint-staged` config for code formatting and type checking
    Added a lint-staged config that includes prettier for all files, eslint with auto-fix for JS/TS/TSX files, and TypeScript compiler checks for TS/TSX files. Additionally, updated tsconfig.eslint.json to include the new lint-staged config file in its file scope.

---
+++ lint-staged.config.js
@@ -0,0 +1,19 @@
+// @ts-check
+
+import path from 'node:path';
+
+/**
+ * @param {string[]} filenames
+ */
+const buildEslintCommand = (filenames) =>
+  `next lint --fix --file ${filenames
+    .map((f) => path.relative(process.cwd(), f))
+    .join(' --file ')}`;
+
+const lintStagedConfig = {
+  '*': 'prettier --ignore-unknown --write',
+  '*.{js,ts,tsx}': [buildEslintCommand],
+  '*.ts?(x)': () => 'tsc -p ./tsconfig.json --force --noEmit',
+};
+
+export default lintStagedConfig;
